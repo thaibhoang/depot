f444ec78c49c2012b73409a7e8293165273f465e
thaibhoang <hoangthai.ftu53@gmail.com>
2024-04-09 14:28:55 +0700

    Add order form, form can change by stimulus

--- test/system/line_items_test.rb
+++ test/system/line_items_test.rb
@@ -21,23 +21,4 @@ class LineItemsTest < ApplicationSystemTestCase
     assert_text "Line item was successfully created"
     click_on "Back"
   end
-
-  test "should update Line item" do
-    visit line_item_url(@line_item)
-    click_on "Edit this line item", match: :first
-
-    fill_in "Cart", with: @line_item.cart_id
-    fill_in "Product", with: @line_item.product_id
-    click_on "Update Line item"
-
-    assert_text "Line item was successfully updated"
-    click_on "Back"
-  end
-
-  test "should destroy Line item" do
-    visit line_item_url(@line_item)
-    click_on "Destroy this line item", match: :first
-
-    assert_text "Line item was successfully destroyed"
-  end
 end
--- test/system/products_test.rb
+++ test/system/products_test.rb
@@ -14,10 +14,10 @@ class ProductsTest < ApplicationSystemTestCase
     visit products_url
     click_on "New product"
 
-    fill_in "Description", with: @product.description
+    fill_in "Description", with: "@product.description"
     fill_in "Image url", with: @product.image_url
     fill_in "Price", with: @product.price
-    fill_in "Title", with: @product.title
+    fill_in "Title", with: "@product.title"
     click_on "Create Product"
 
     assert_text "Product was successfully created"
@@ -31,7 +31,7 @@ class ProductsTest < ApplicationSystemTestCase
     fill_in "Description", with: @product.description
     fill_in "Image url", with: @product.image_url
     fill_in "Price", with: @product.price
-    fill_in "Title", with: @product.title
+    fill_in "Title", with: "@product.title"
     click_on "Update Product"
 
     assert_text "Product was successfully updated"
