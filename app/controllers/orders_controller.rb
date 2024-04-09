2a9720581e6cac9b8172e5e1d6f3a7a2a38ad84a
thaibhoang <hoangthai.ftu53@gmail.com>
2024-04-09 17:38:26 +0700

    Hide checkout button on order form

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -16,6 +16,7 @@ class OrdersController < ApplicationController
   # GET /orders/new
   def new
     @order = Order.new
+    @on_order_page = true
   end
 
   # GET /orders/1/edit
---
+++ app/javascript/controllers/disable_checkout_controller.js
@@ -0,0 +1,36 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="disable-checkout"
+export default class extends Controller {
+  static targets = ["checkout"]
+
+  initialize() {
+    this.setupTurboStreamListener()  
+     
+  }
+
+  setupTurboStreamListener() {
+    
+    document.addEventListener('turbo:load', () => {
+      // disable when first visit the order page
+      this.disableCheckout() 
+      const cartDiv = document.getElementById('cart');
+      if (cartDiv) {        
+        window.addEventListener('cartLoaded', () => {
+          console.log(this.checkoutTarget) 
+          // disable everytime cart is reloaded
+          this.disableCheckout();
+        });
+      }
+    })
+  }
+
+  inOrderPage() {
+    const order_form = document.querySelector("#order-form")
+    return order_form != null
+  }
+
+  disableCheckout() {
+    this.checkoutTarget.disable = this.checkoutTarget.hidden = this.inOrderPage()
+  }
+}
